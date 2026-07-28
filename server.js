8ba97921473eddcfbea9f3fd243f3c9cc8aff8c9
Khoshe <123952664+Khoshe@users.noreply.github.com>
2026-07-28 20:32:16 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -6,9 +6,31 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
+// Een teller om het aantal openstaande bestellingen bij te houden
+let openOrdersCount = 0;
+
 io.on('connection', (socket) => {
+    console.log('Gebruiker verbonden');
+    
+    // Stuur direct de huidige wachtrij-stand naar de nieuwe gebruiker
+    socket.emit('queue-update', openOrdersCount);
+
+    // Als er een nieuwe bestelling binnenkomt
     socket.on('new-order', (orderData) => {
+        openOrdersCount++;
+        // Stuur de bestelling naar het bar-dashboard
         io.emit('bar-receive-order', orderData);
+        // Stuur de nieuwe wachtrij-stand naar álle telefoons van je vrienden
+        io.emit('queue-update', openOrdersCount);
+    });
+
+    // Als de bartender een bestelling afhandelt (op 'Klaar' klikt)
+    socket.on('order-completed', () => {
+        if (openOrdersCount > 0) {
+            openOrdersCount--;
+        }
+        // Stuur de vernieuwde lagere wachtrij-stand naar iedereen
+        io.emit('queue-update', openOrdersCount);
     });
 });
 
